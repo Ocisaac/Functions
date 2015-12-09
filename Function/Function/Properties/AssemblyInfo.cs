f14430944a5740a1117e48fa1d6754d04c4053ba
Ocisaac <aniVVT@gmail.com>
2015-12-09 19:09:10 +0200

    Edited assembly propreties

--- Function/Function/Properties/AssemblyInfo.cs
+++ Function/Function/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Function")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Functions")]
+[assembly: AssemblyDescription("Functional-programming like functions in C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Function")]
+[assembly: AssemblyProduct("Functions")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
